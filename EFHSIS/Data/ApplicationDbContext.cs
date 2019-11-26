5e697d9287e2be049ed69c7efa3e209b87352027
Keith <keithjoseph97@gmail.com>
2019-11-26 13:00:22 +0800

    first commit

---
+++ EFHSIS/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EFHSIS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFHSIS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User,IdentityRole<int>,int>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
